b96ef7931ddaaadd14d8f0d39d41c6f451eca4ec
Charles Harries <charlesgharries@gmail.com>
2019-07-13 19:52:00 +0100

    Add bits

---
+++ pages/bits.js
@@ -0,0 +1,36 @@
+const bits = [
+  {
+    link: 'https://squarez.charlesharri.es',
+    date: "Jul '19",
+    title: 'Squarez',
+    description: 'Learn math, angles, and funky CSS together with me.',
+  },
+  {
+    link: 'https://feeling.charlesharri.es',
+    date: "Jun '19",
+    title: 'Feeling',
+    description:
+      'A robot on the Internet that can tell exactly how you feel based on what you tell it.',
+  },
+];
+
+function Bits() {
+  return (
+    <main className="Bits">
+      <h1 className="Bits__title">Various Bits</h1>
+      <ul>
+        {bits.map(bit => (
+          <a key={bit.title} href={bit.link}>
+            <p className="Bit__title">
+              <strong>{bit.title}</strong>
+              <span className="Bit__date">{bit.date}</span>
+            </p>
+            <p className="Bit_description">{bit.description}</p>
+          </a>
+        ))}
+      </ul>
+    </main>
+  );
+}
+
+export default Bits;

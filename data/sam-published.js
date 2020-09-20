3f932c87d1d7c66fcf3071fa7dd54534d20d10c0
Charles Harries <charles@harries.me>
2020-09-20 21:20:20 +0100

    Add usps-and-corruption

--- data/sam-published.js
+++ data/sam-published.js
@@ -1,5 +1,10 @@
 export default [
   {
+    title: 'USPS & Corruption',
+    date: "25 Aug '20",
+    slug: 'usps-and-corruption',
+  },
+  {
     title: 'A-levels and fuck the Tories',
     date: "18 Aug '20",
     slug: 'a-levels-and-fuck-the-tories',

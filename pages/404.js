69db374aa71d5ce163507f35c74efefb8a27ce66
Charles Harries <charles@harries.me>
2021-01-24 19:00:02 +0000

    Update 404 page

--- pages/404.js
+++ pages/404.js
@@ -1,3 +1,11 @@
+import Link from 'next/link';
+
 export default function Error404() {
-  return <h1>Not found</h1>;
+  return (
+    <div className="stack">
+      <h1>404</h1>
+      <p>Couldn't find that one.</p>
+      <Link href="/">Back home</Link>
+    </div>
+  );
 }

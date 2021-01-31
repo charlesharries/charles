635d38e9eef849a78cc81c23020cfa1a6aa5b905
Charles Harries <charles@harries.me>
2021-01-31 17:49:20 +0000

    Refactor date component

--- components/Date/index.js
+++ components/Date/index.js
@@ -1,11 +1,60 @@
 import PropTypes from 'prop-types';
 
-function Date({ children }) {
-  return <p style={{ fontStyle: 'italic' }}>{children}</p>;
+/**
+ * Get the English month for the given date.
+ *
+ * @param {Date} d - The date to get the month for.
+ */
+function month(d) {
+  const months = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+  ];
+
+  return months[d.getMonth()];
 }
 
-Date.propTypes = {
+/**
+ * Format a date like "1 January 2021".
+ *
+ * @param   {Date} date - The date to format.
+ * @returns {string}
+ */
+const format = date => {
+  const y = date.getFullYear() % 100;
+  const m = month(date);
+  const d = date.getDate();
+
+  return `${d} ${m} '${y}`;
+};
+
+function DateComponent({ children, date: dateString }) {
+  const style = { fontStyle: 'italic' };
+
+  if (children) {
+    return <p style={style}>{children}</p>;
+  }
+
+  return (
+    <p style={style}>
+      <time dateTime={dateString}>{format(new Date(dateString))}</time>
+    </p>
+  );
+}
+
+DateComponent.propTypes = {
+  date: PropTypes.string,
   children: PropTypes.oneOfType([PropTypes.element, PropTypes.string]),
 };
 
-export default Date;
+export default DateComponent;

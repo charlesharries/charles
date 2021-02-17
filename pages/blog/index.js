a26a39502bc16fd67651b33487247e9cc5c577cd
Charles Harries <charles@harries.me>
2021-02-17 21:30:44 +0000

    Add dates to all posts

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,20 +1,41 @@
 import Link from 'next/link';
+import PropTypes from 'prop-types';
 import published from '~data/published';
+
+/**
+ * A single blog item.
+ *
+ * @todo Once we've got all of the dates set, use the DateComponent here.
+ * @param {{ slug: string, title: string, date: string }} post
+ */
+function BlogItem({ slug, title, date }) {
+  return (
+    <li>
+      <article>
+        <Link href={`/blog/${slug}`}>
+          <a className="BlogLink t-para">
+            <span className="BlogLink__date t-small">{date}</span>
+            <span className="BlogLink__text">{title}</span>
+          </a>
+        </Link>
+      </article>
+    </li>
+  );
+}
+
+BlogItem.propTypes = {
+  slug: PropTypes.string,
+  title: PropTypes.string,
+  date: PropTypes.string,
+};
 
 function Blog() {
   return (
     <div className="Blog">
-      <h1 className="Blog__title">infrequent thoughts</h1>
+      <h1 className="Blog__title">the blog</h1>
       <ul>
-        {published.map(post => (
-          <li key={post.slug}>
-            <Link href={`/blog/${post.slug}`}>
-              <a className="BlogLink t-para">
-                <span className="BlogLink__text">{post.title}</span>
-                <span className="BlogLink__date t-small">{post.date}</span>
-              </a>
-            </Link>
-          </li>
+        {published.map(p => (
+          <BlogItem {...p} />
         ))}
       </ul>
     </div>

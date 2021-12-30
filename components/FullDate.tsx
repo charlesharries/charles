7de7da5cd1ada6a943738baf5e94cde75a954352
Charles Harries <charles@harries.me>
2021-12-30 00:18:52 +0000

    Fix issue parsing datetime

--- components/FullDate.tsx
+++ components/FullDate.tsx
@@ -1,7 +1,7 @@
 import { longDate, time } from '../util/date';
 
 export default function FullDate({ datetime }) {
-  const date = new Date(datetime.replace(/-/g, '/').split('.')[0]);
+  const date = new Date(datetime);
 
   return (
     <time dateTime={date.toISOString()}>

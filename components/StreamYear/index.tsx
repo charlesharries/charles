41031e8386faaa5d2286cb532a6e0744413830bb
Charles Harries <charles@harries.me>
2022-05-22 12:19:32 +0100

    Wrap archive stream links
    Closes #59

--- components/StreamYear/index.tsx
+++ components/StreamYear/index.tsx
@@ -17,12 +17,12 @@ export default function StreamYear({ year, months }: Props): JSX.Element {
   return (
     <div className="StreamYear">
       <h3>{year}</h3>
-      <ul className="stack stack--sm">
+      <ul className="stack">
         {sortedMonths.map((month) => (
           <li className="cluster" key={`${year} ${month}`}>
             <h4 className="w-10 mobile:w-100">{fullMonth(parseInt(month))}</h4>
             <div className="flex-1">
-              <ul className="cluster cluster--sm" style={separatorStyle}>
+              <ul className="cluster cluster--sm cluster--list" style={separatorStyle}>
                 {months[month].map((post) => (
                   <li key={post.title}>
                     <Link href={`/stream/${post.slug}`}>
